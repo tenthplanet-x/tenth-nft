84bb4d384435b6b0aaff16c90c90fcd7b060ac25
shijie <chishijie85@gmail.com>
2022-08-29 12:12:52 +0800

    Adjust web3 wallet balance api

---
+++ nft-convention/src/main/solidity/WETH9.wrapper.sh
@@ -0,0 +1,2 @@
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/zulu-8.jdk/Contents/Home
+web3j generate solidity -b /Users/chishijie/Documents/tpulse/java/tpulse-nft/nft-convention/src/main/solidity/WETH9.bin -a /Users/chishijie/Documents/tpulse/java/tpulse-nft/nft-convention/src/main/solidity/WETH9.abi -o /Users/chishijie/Documents/tpulse/java/tpulse-nft/nft-convention/src/main/java -p com.tenth.nft.solidity --contractName=WETHContract
