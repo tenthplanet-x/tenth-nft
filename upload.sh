5c5175b78ef352b90b03c01d7f4370ddd7482b30
shijie <chishijie85@gmail.com>
2022-09-29 15:43:13 +0800

    Modify upload.sh

--- upload.sh
+++ upload.sh
@@ -1,13 +1,17 @@
 
 ./gradlew :nft-convention:clean :nft-convention:publish -x test
 ./gradlew :nft-crawler:clean :nft-crawler:publish -x test
-./gradlew :nft-marketplace-exchange:clean :nft-marketplace-exchange:publish -x test
-./gradlew :nft-marketplace:clean :nft-marketplace:publish -x test
 ./gradlew :nft-operation:clean :nft-operation:publish -x test
 ./gradlew :nft-convention:clean :nft-convention:publish -x test
-./gradlew :nft-search:clean :nft-search:publish -x test
 ./gradlew :nft-orm:clean :nft-orm:publish -x test
 ./gradlew :nft-player:clean :nft-player:publish -x test
-./gradlew :nft-wallet:clean :nft-wallet:publish -x test
-./gradlew :nft-web3:clean :nft-web3:publish -x test
 
+./gradlew :nft-wallet-buildin:clean :nft-wallet-buildin:publish -x test
+./gradlew :nft-wallet-web3:clean :nft-wallet-web3:publish -x test
+
+./gradlew :nft-marketplace:clean :nft-marketplace:publish -x test
+./gradlew :nft-marketplace-common:clean :nft-marketplace-common:publish -x test
+./gradlew :nft-marketplace-web3:clean :nft-marketplace-web3:publish -x test
+./gradlew :nft-marketplace-buildin:clean :nft-marketplace-buildin:publish -x test
+./gradlew :nft-marketplace-stats:rec :nft-marketplace-stats:publish -x test
+./gradlew :nft-marketplace:clean :nft-marketplace:publish -x test
